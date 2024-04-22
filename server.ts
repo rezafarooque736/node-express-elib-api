55d5682818284e7df02b11016b07604c8d916f45
Farooque Reza <faizan.reza3@gmail.com>
2024-04-22 11:56:53 +0530

    add database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
+import connectToMongoDB from "./src/config/db";
 
-const startServer = () => {
+const startServer = async () => {
+  // connect to mongodb database
+  await connectToMongoDB();
+
   const port = config.port;
 
   app.listen(port, () => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ conf();
 
 const _config = {
   port: process.env.PORT || 3000,
+  databaseUrl: process.env.MONGODB_CONNECTION_STRING,
 };
 
 export const config = Object.freeze(_config);

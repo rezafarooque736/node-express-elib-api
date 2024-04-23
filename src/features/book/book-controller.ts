5b3a58a568058b6d295017ab9686fb205d3d1e1c
Farooque Reza <faizan.reza3@gmail.com>
2024-04-23 21:26:36 +0530

    upload files to cloud

--- src/features/book/book-controller.ts
+++ src/features/book/book-controller.ts
@@ -1,14 +1,62 @@
+import path from "node:path";
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../../config/cloudinary";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { title, author, description, image } = req.body;
+    console.log("files", req.files);
+
+    const files = req.files as { [filename: string]: Express.Multer.File[] };
+
+    // upload cover image
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const coverImageFileName = files.coverImage[0].filename;
+    const coverImageFilePath = path.resolve(
+      __dirname,
+      "../../../public/data/uploads",
+      coverImageFileName
+    );
+
+    const coverImageUploadResult = await cloudinary.uploader.upload(
+      coverImageFilePath,
+      {
+        filename_override: coverImageFileName,
+        folder: "book-covers",
+        format: coverImageMimeType,
+      }
+    );
+
+    // upload book file
+    const bookMimeType = files.file[0].mimetype.split("/").at(-1);
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../../public/data/uploads",
+      coverImageFileName
+    );
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: bookMimeType,
+      }
+    );
+
+    console.log({ coverImageUploadResult, bookFileUploadResult });
 
     res.status(201).json({
       message: "Book created successfully",
     });
   } catch (error) {
-    next(error);
+    const err = createHttpError(
+      500,
+      "Error while uploading files to cloudinary"
+    );
+    next(err);
   }
 };
 
--- src/features/book/book-router.ts
+++ src/features/book/book-router.ts
@@ -9,15 +9,15 @@ const bookRouter = Router();
 const upload = multer({
   dest: path.resolve(__dirname, "../../../public/data/uploads"),
   limits: {
-    fileSize: 1024 * 1024 * 3, //3MB
+    fileSize: 10 * 1024 * 1024, //10MB
   },
 });
 
 bookRouter.post(
   "/",
   upload.fields([
-    { name: "coverImage", maxCount: 1 },
     { name: "file", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
   ]),
   createBook
 );
